429e7e16f09c67baa73753fbd87ddfcef52e01f5
cosm_akylka <cosmyti228@gmail.com>
2025-02-14 23:12:00 +0000

    deleting everything

--- inc/menu.h
+++
@@ -1,9 +0,0 @@
-#ifndef MENU_H
-#define MENU_H
-
-#include "main.h"
-
-void DrawMainMenu(Rectangle playButton, Rectangle creditsButton, Rectangle quitButton, Vector2 mousePos);
-void DrawCredits(Rectangle backButton, Vector2 mousePos);
-
-#endif
--- resources/rocket_game.h
+++
@@ -1,9 +0,0 @@
-#ifndef ROCKET_GAME_H
-#define ROCKET_GAME_H
-
-void InitRocketGame(void);
-void UpdateRocketGame(void);
-void DrawRocketGame(void);
-void CloseRocketGame(void);
-
-#endif
